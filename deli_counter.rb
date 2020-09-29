321b29cd27c9e3d3e4e27d25acb681d30331abd9
spectacular-command-6417 <chrsrsr@gmail.com>
2020-09-29 20:04:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+
+ Chris's_cool_kickz = [ ]
+ 
+  return_a_ticket(Chris's_cool_kickz, "dashawn") #=> Welcome, dashawn. You are number 1 in line.
+  return_a_ticket(Chris's_cool_kickz, "Javier") #=> Welcome, Javier. You are number 2 in line.
+  return_a_ticket(Chris's_cool-kickz, "John") #=> Welcome, John. You are number 3 in line.
+  
+  
